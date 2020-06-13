daab770f737866e0f10e7188ac9a57eaf717f8f7
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-06-13 23:15:45 +0900

    Added get languages function

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const { Translate } = require('@google-cloud/translate').v2;
 const translate = new Translate({ credentials: googleCreds });
 
 async function translateText(text, target) {
-    let translations = await translate.translate(text, target);
-    console.log(translations,)
-    // translations = Array.isArray(translations) ? translations : [translations];
-    // console.log('Translations:');
-    // translations.forEach((translation, i) => {
-    //     console.log(`${text[i]} => (${target}) ${translation}`);
-    // });
+    const [translations, otherInfo] = await translate.translate(text, target);
+    console.log(translations, otherInfo.data)
+    return translations;
 };
+
+async function getLanguages(target) {
+    const [languages, otherInfo] = await translate.getLanguages(target && target);
+    console.log(languages, otherInfo.data);
+    return languages;
+}
 
 const text = 'Hello, world!';
 const target = 'ja';
 
 (async () => {
     await translateText(text, target);
+    await getLanguages("ja");
+    await getLanguages();
 })();
